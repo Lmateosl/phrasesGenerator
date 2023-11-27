8059aeb85f8111f5c54d6ac277c999f6164ee1f0
mateo <mateosl@enerwicorp.com>
2023-11-27 00:06:21 -0500

    index.js creat

---
+++ index.js
@@ -0,0 +1,9 @@
+const phrases = {
+    _parts: {
+        subject: ["The gold", "Life", "The dream", "The succes"],
+        verb: ["is", "have", "understands", "comprise"],
+        article: ["several", "some", "sundry", "many"],
+        complement: ["steps", "cycles", "periods", "stages", "phases", "lapses"],
+         
+    }
+}
