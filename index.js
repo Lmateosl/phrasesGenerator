9bf2c2629bedfb9c7a5278f3d27a0d054e7b1c92
mateo <mateosl@enerwicorp.com>
2023-11-27 13:32:41 -0500

    phrase generator ready

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const phrases = {
         verb: ["is", "have", "understands", "comprise"],
         article: ["several", "some", "sundry", "many"],
         complement: ["steps", "cycles", "periods", "stages", "phases", "lapses"],
-         
+        complement2: ["to succeed", "to achive", "to triumph", "to thrive" ]
+    },
+    get subject () {
+        return this._parts.subject[Math.floor(Math.random() * this._parts.subject.length)];
+    },
+    get verb () {
+        return this._parts.verb[Math.floor(Math.random() * this._parts.verb.length)];
+    },
+    get article () {
+        return this._parts.article[Math.floor(Math.random() * this._parts.article.length)];
+    },
+    get complement () {
+        return this._parts.complement[Math.floor(Math.random() * this._parts.complement.length)];
+    },
+    get complement2 () {
+        return this._parts.complement2[Math.floor(Math.random() * this._parts.complement2.length)];
     }
 }
+
+const generator = () => {
+    const phraseArray = Object.keys(phrases._parts).map(part => phrases[part]);
+    return phraseArray.join(" ");
+}
+
+console.log(generator());
